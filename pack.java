457331dcd7ade914df44103d0c43cb340065ddd5
markdcayanan-a11y <markdcayanan@gmail.com>
2026-04-05 23:46:25 +0800

    Add files via upload

---
+++ pack.java
@@ -0,0 +1,18 @@
+package idk;
+public class kaibigan{
+	
+	String name;
+	int age;
+	char gender;
+	String hobby;
+	
+kaibigan(String name, int age, char Gender, String Hobby){
+this.name = name;
+this.age = age;
+this.gender = gender;
+this.hobby = hobby;
+
+System.out.println("Name: "+name+"/n Age: "+age+"/n Gender:"+gender+"/n Hobby: ");
+
+}
+}
